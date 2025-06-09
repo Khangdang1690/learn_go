fee76a2a9d54ea55d6f1348008f10da3282a4081
Khang Dang <khangdang0199@gmail.com>
2025-06-08 23:00:07 -0700

    Done fundamental

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "learn_go/testpackage"
+import (
+	"fmt"
+	"learn_go/testpackage"
+)
 
 func main() {
-	testpackage.MyFunction()
+	var step int = 5
+	var name string = "learn_go"
+	var left string = "12"
+	var right string = "10"
+	var isHappy bool = true
+	testpackage.MyFunction(step)
+	fmt.Println(name)
+	fmt.Println(left + right)
+	fmt.Println(isHappy)
 }
--- testpackage/test.go
+++ testpackage/test.go
@@ -2,10 +2,18 @@ package testpackage
 
 import "fmt"
 
-func MyFunction() {
+func MyFunction(step int) {
+	if step == 1 {
+		fmt.Println("step 1")
+	} else if step == 2 {
+		fmt.Println("step 2")
+	} else {
+		fmt.Println("step not supported")
+	}
 	fmt.Println("Hello Go!")
+	myPrivateFunction()
 }
 
 func myPrivateFunction() {
-	fmt.Println("Hello Go!")
+	fmt.Println("Hello Bro!")
 }
