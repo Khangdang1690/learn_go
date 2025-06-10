79ea4bbfb044d425b19f6e2eeda69070b7ea7681
Khang Dang <khangdang0199@gmail.com>
2025-06-09 22:14:14 -0700

    End

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module learn_go
 
 go 1.24.4
+
+require github.com/stripe/stripe-go/v82 v82.2.1 // indirect
--- main.go
+++ main.go
@@ -1,18 +1,101 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
-	"learn_go/testpackage"
+	"github.com/stripe/stripe-go/v82"
+	"github.com/stripe/stripe-go/v82/paymentintent"
+	"io"
+	"log"
+	"net/http"
 )
 
 func main() {
-	var step int = 5
-	var name string = "learn_go"
-	var left string = "12"
-	var right string = "10"
-	var isHappy bool = true
-	testpackage.MyFunction(step)
-	fmt.Println(name)
-	fmt.Println(left + right)
-	fmt.Println(isHappy)
+	stripe.Key = ""
+	http.HandleFunc("/create-payment-intent", handleCreatePaymentIntent)
+	http.HandleFunc("/health", handleHealth)
+	log.Println(("Listening on port 4242..."))
+	var err error = http.ListenAndServe("localhost:4242", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func handleCreatePaymentIntent(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+		return
+	}
+
+	var req struct {
+		ProductId string `json:"product_id"`
+		FirstName string `json:"first_name"`
+		LastName  string `json:"last_name"`
+		Address1  string `json:"address_1"`
+		Address2  string `json:"address_2"`
+		City      string `json:"city"`
+		State     string `json:"state"`
+		Zip       string `json:"zip"`
+		Country   string `json:"country"`
+	}
+
+	err := json.NewDecoder(r.Body).Decode(&req)
+
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+		return
+	}
+
+	params := &stripe.PaymentIntentParams{
+		Amount:   stripe.Int64(calculateOrderAmount(req.ProductId)),
+		Currency: stripe.String(string(stripe.CurrencyUSD)),
+		AutomaticPaymentMethods: &stripe.PaymentIntentAutomaticPaymentMethodsParams{
+			Enabled: stripe.Bool(true),
+		},
+	}
+	paymentIntent, err := paymentintent.New(params)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	fmt.Println(paymentIntent.ClientSecret)
+	var response struct {
+		ClientSecret string `json:"client_secret"`
+	}
+	response.ClientSecret = paymentIntent.ClientSecret
+	var buf bytes.Buffer
+	json.NewEncoder(&buf).Encode(response)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+
+	_, err = io.Copy(w, &buf)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func handleHealth(w http.ResponseWriter, r *http.Request) {
+	response := []byte("Server is up and running!")
+
+	_, err := w.Write(response)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func calculateOrderAmount(productId string) int64 {
+	switch productId {
+	case "Forever Pants":
+		return 26000
+	case "Forever Shirt":
+		return 15500
+	case "Forever Shorts":
+		return 30000
+	}
+	return 0
 }
